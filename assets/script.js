0c19780625665ed6c71f3ad0c84f58aea7ab96b1
Jaime M <jaimemarsh19@gmail.com>
2023-03-15 14:08:35 -0400

    added == true to confirms which madde the specifications work. Really excited to complete this

--- assets/script.js
+++ assets/script.js
@@ -16,30 +16,39 @@ generateBtn.addEventListener("click", writePassword);
 //make generatePassword()
 
 function generatePassword() {
-  let length = 8;
-  let choiceArry = [];
-  let specialCharactersArry = ['!', '@', '$', '%', '?', '<', '>', '*', '#', ';',];
-  let lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  let uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X','Y','Z'];
-  let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-
-
-
   console.log("Click")
-  prompt("Please select a length for your password 8-128 characters");
-  //
-  prompt("Would you like lowercase letters?");
-  prompt("Would you like uppercase letters?");
-  prompt("Would you like any special characters?");
 
-  //1. Prompt the user for the password critera
-  // a. password length 8 < 128
-  // b. do you want to include lowercase, upper case, special characters
-  //2. validate the input, make sure everything matches
-  //3. generate password based on critera
+  let characterLength = "";
+  let choiceArr = [];
+  let specialCharactersArr = ['!', '@', '$', '%', '?', '<', '>', '*', '#', ';',];
+  let lowercaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let uppercaseArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let numberArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let password = "";
 
+  characterLength = parseInt(prompt("Please select a length for your password 8-128 characters"));
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please try again and choose a number 8 - 128");
+    return "Try Again";
+  }
+  if (confirm("Would you like lowercase letters") == true) {
+    choiceArr = choiceArr.concat(lowercaseArr);
+  }
+  if (confirm("Would you like uppercase letters") == true) {
+    choiceArr = choiceArr.concat(uppercaseArr);
+  }
+  if (confirm("Would you like any special characters") == true) {
+    choiceArr = choiceArr.concat(specialCharactersArr);
+  }
+  if (confirm("Would you like numbers?") == true) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  console.log(choiceArr)
 
-  //4. display password on page
-  return "Generated password will go here"
+  for (var i = 0; i < characterLength; i++) {
+    let randomGenerator = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomGenerator];
+  }
+
+  return password
 }
